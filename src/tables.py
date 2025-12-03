c1d28656f54caba90000cf97ea5c0d91558620fd
SorenWithAGit <john.sorensen@usda.gov>
2025-12-03 16:08:07 -0600

    added class tables with add, clear, and delete functions

---
+++ src/tables.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+class Tables:
+    def __init__(self, database):
+        self.database = database
+        self.conn = sqlite3.connect(self.database)
+        self.cur = self.conn.cursor()
+    
+    def add_Table(self, table_name, attribute_names, attribute_types):
+        tables_script = str("CREATE TABLE " + table_name + "(ID INTEGER PRIMARY KEY,")
+        for i, attribute in enumerate(attribute_names):
+            tables_script += str(attribute +  " " + attribute_types[i] + ",")
+        tables_script += ");"
+        tables_script = tables_script.replace(",);", ");")
+        print(tables_script)
+        # self.cur.executescript(tables_script)
+        # self.conn.commit()
+
+    def clear_Table(self, table_names):
+        for table in table_names:
+            clear_script = ""
+            clear_script += str("TRUNCATE TABLE" + table)
+        self.cur.executescript(clear_script)
+        self.conn.commit()
+
+    def delete_Table(self, table_names):
+        for table in table_names:
+            delete_script = ""
+            delete_script += str("DELETE TABLE" + table)
+        self.cur.executescript(delete_script)
+        self.conn.commit()
+
+
+
+
