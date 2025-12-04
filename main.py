b3e2fea59c27ae4666da0dc910a4558135559a35
SorenWithAGit <john.sorensen@usda.gov>
2025-12-04 09:37:24 -0600

    arranged docstrings

--- main.py
+++ main.py
@@ -3,23 +3,39 @@ from src import tables
 
 
 """
-To add tables to the database utilize the tables module and class Tables.
-The add_Table function accepts three parameters: Table Name, list of Attributes, and
-list of Attribute Types.
+#######################################################################
+
+To add tables to the database utilize the tables module and class 
+Tables.
+The add_Table function accepts three parameters: Table Name, list of
+Attributes, and list of Attribute Types.
 
 An example being:
 attributes = ["Field", "[Size (acre)]", "[Size (ha)]", "Landuse"]
-attribute_types = ["TEXT", "FLOAT", "FLOAT", "INTEGER REFERENCES Landuse (ID)"]
+attribute_types = ["TEXT", "FLOAT", "FLOAT", "INTEGER REFERENCES 
+                    Landuse (ID)"]
 tb = tables.Tables(DB)
 tb.add_Table("Fields", attributes, attribute_types)
 
+------------------------------------------------------------------------
 
-To clear tables in the database utilize the tables module and class Tables.
+To clear tables in the database utilize the tables module and class 
+Tables.
 The clear_Table function accepts a list of table names to clear.
 
+An example being:
+tb.clear_Table(["[Field Boundaries]"])
 
-To delete tables in the database utilize the tables module and class Tables.
-the delete_Table function accepts a list of table names to delete.
+------------------------------------------------------------------------
+
+To delete tables in the database utilize the tables module and class 
+Tables. the delete_Table function accepts a list of table names to
+delete.
+
+An example being:
+tb.delete_Table(["[Field Boundaries]"])
+
+#######################################################################
 """
 
 # Create Database
