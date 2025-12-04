02ce50bd68e636f0be18c200a1d2668a2c91d9a0
SorenWithAGit <john.sorensen@usda.gov>
2025-12-04 07:37:54 -0600

    modified sql statements for tables

--- src/tables.py
+++ src/tables.py
@@ -7,7 +7,7 @@ class Tables:
         self.cur = self.conn.cursor()
     
     def add_Table(self, table_name, attribute_names, attribute_types):
-        tables_script = str("CREATE TABLE " + table_name + "(ID INTEGER PRIMARY KEY,")
+        tables_script = str("CREATE TABLE " + table_name + " (ID INTEGER PRIMARY KEY,")
         for i, attribute in enumerate(attribute_names):
             tables_script += str(attribute +  " " + attribute_types[i] + ",")
         tables_script += ");"
@@ -19,14 +19,14 @@ class Tables:
     def clear_Table(self, table_names):
         for table in table_names:
             clear_script = ""
-            clear_script += str("TRUNCATE TABLE" + table)
+            clear_script += str("TRUNCATE TABLE " + table)
         self.cur.executescript(clear_script)
         self.conn.commit()
 
     def delete_Table(self, table_names):
         for table in table_names:
             delete_script = ""
-            delete_script += str("DELETE TABLE" + table)
+            delete_script += str("DELETE TABLE " + table)
         self.cur.executescript(delete_script)
         self.conn.commit()
 
