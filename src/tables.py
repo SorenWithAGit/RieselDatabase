0b235a8711bc9e5e98ade82f61e2bb2624660809
SorenWithAGit <john.sorensen@usda.gov>
2025-12-05 10:32:14 -0600

    defined table fn argument types

--- src/tables.py
+++ src/tables.py
@@ -6,7 +6,7 @@ class Tables:
         self.conn = sqlite3.connect(self.database)
         self.cur = self.conn.cursor()
     
-    def add_Table(self, table_name, attribute_names, attribute_types):
+    def add_Table(self, table_name: str, attribute_names: list, attribute_types: list):
         tables_script = str("CREATE TABLE " + table_name + " (ID INTEGER PRIMARY KEY,")
         for i, attribute in enumerate(attribute_names):
             tables_script += str(attribute +  " " + attribute_types[i] + ",")
@@ -16,14 +16,14 @@ class Tables:
         self.cur.executescript(tables_script)
         self.conn.commit()
 
-    def clear_Table(self, table_names):
+    def clear_Table(self, table_names: str):
         for table in table_names:
             clear_script = ""
             clear_script += str("TRUNCATE TABLE " + table)
         self.cur.executescript(clear_script)
         self.conn.commit()
 
-    def delete_Table(self, table_names):
+    def delete_Table(self, table_names: str):
         for table in table_names:
             delete_script = ""
             delete_script += str("DROP TABLE " + table)
