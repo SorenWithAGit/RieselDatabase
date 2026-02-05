8626b5d5d8a5d55280a94dc52e15cb8322d8933d
John Sorensen <john.sorensen@usda.gov>
2026-02-05 16:17:45 -0600

    reads yearly weather files, compares to second location

---
+++ src/fileChecker.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import os
+import glob
+
+
+class read_excel:
+
+    def read_weather(filepath: str):
+        headers = ["mo", "day", "year", "Max Temp (C)", "Min Temp (C)", "Precip (mm)"]
+        df = pd.read_excel(filepath)
+        df = df.drop(index = [0, 1, 2, 3])
+        df_columns = df.columns.tolist()
+        column_dict = dict(zip(df_columns, headers))
+        df = df.rename(columns = column_dict)
+        df = df.reset_index(drop = True)
+        return df
+
+class file_checker:
+    
+    def check_weather_files(file1: str, file2: str, df1: pd.DataFrame, df2: pd.DataFrame):
+        fn1 = os.path.basename(file1).split("/")[-1]
+        fn2 = os.path.basename(file2).split("/")[-1]
+        if df1.equals(df2):
+            print("Harmel: " + fn1 + " is identical to Umbraco: " + fn2)
+            
+        else:
+            print("Harmel: " + fn2 + " is not identical to Umbraco: " + fn2)
+
+folder1 = r"\\ARS-DATA\Archive\HarmelExit\riesel\weather\koesterdailyweather"
+folder2 = r"I:\programming\python\riesel_file_checker\Umbraco Website Files"
+
+path1 = glob.glob(folder1 + "//" + "*ries.xls")
+path2 = glob.glob(folder2 + "//" + "*ries.xls")
+files1 = []
+files2 = []
+
+for path in path1:
+    file = os.path.basename(path).split("/")[-1]
+    files1.append(file)
+
+for path in path2:
+    file = os.path.basename(path).split("/")[-1]
+    files2.append(file)
+
+files2 = [file for file in files1 if file in files2]
+
+if files1 == files2:
+    print("files match")
+
+for p, path in enumerate(path1):
+    re = read_excel
+    f1 = re.read_weather(path)
+    # print("Harmel File: ")
+    # print(f1)
+    f2 = re.read_weather(path2[p])
+    # print("Umbraco Website File: ")
+    # print(f2)
+
+    fc = file_checker
+    fc.check_weather_files(path, path2[p], f1, f2)
