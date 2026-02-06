17e1090bbb4af9aaa33be6fa5d2bd11ec5c49b51
John Sorensen <john.sorensen@usda.gov>
2026-02-06 14:56:20 -0600

    read_precip fn tested on Harmel subdaily

--- src/fileChecker.py
+++ src/fileChecker.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import glob
+import csv
 
 
 class files:
@@ -48,6 +49,81 @@ class read_excel:
         df["Date"] = pd.to_datetime(df[["year", "month", "day"]])
         df = df.iloc[:, [7, 3, 4, 5, 6]]
         return df
+    
+
+    def read_precip(file_path: str):
+        # create blank list to append rows to
+        rows = []
+        # open web file and convert to csv, append each row
+        with open(file_path) as csvconvert:
+            csvread = csv.reader(csvconvert)
+            for row in csvread:
+                rows.append(row[0].split())
+                # print(row[0].split())
+
+        # create pandas dataframe and format columns
+        rain_df = pd.DataFrame(columns = ["Raingauge", "Date", "Time (min)", "Precip (in)"]).astype({"Raingauge" : str,
+                                                                                                "Date" : str,
+                                                                                                "Time (min)" : str,
+                                                                                                "Precip (in)" : float})
+
+        # create empty lists to append data
+        raingauge = []
+        dates = []
+        time = []
+        precip = []
+
+        # from row append data to corresponding list
+        for row in rows[2:]:
+            # print(row)
+            raingauge.append(row[0])
+            date = str(row[1] + "/" + row[2]  + "/" + row[3])
+            dates.append(date)
+            time.append(row[4])
+            precip.append(row[5])
+
+        # add data from lists to dataframe
+        rain_df["Raingauge"] = raingauge
+        rain_df["Date"] = dates
+        rain_df["Time (min)"] = time
+        rain_df["Precip (in)"] = precip
+        rain_df["Precip (in)"] = rain_df["Precip (in)"].astype("float")
+
+
+        # print(rain_df)
+
+        # convert date column to datetime
+        rain_df["Date"] = pd.to_datetime(rain_df["Date"])
+        # print(rain_df)
+
+
+        # create new dataframe with daily values
+        daily_df = rain_df.groupby([
+            rain_df["Date"].dt.year,
+            rain_df["Date"].dt.month,
+            rain_df["Date"].dt.day
+        ]).last().reset_index(drop = True)
+        daily_df["Precip (in)"] = daily_df["Precip (in)"].astype("float")
+        daily_df["Cumulative Precip (in)"] = daily_df["Precip (in)"].cumsum()
+        # print(daily_df)
+        # print(sum(daily_df["Precip (in)"]))
+
+
+
+        
+        buffer_df = daily_df
+        buffer_df = buffer_df.set_index(buffer_df["Date"])
+        buffer_df["Monthly Precip (in)"] = buffer_df.groupby([buffer_df.index.year, buffer_df.index.month])["Precip (in)"].cumsum()
+        # print(buffer_df)
+
+        # create new dataframe with daily values
+        monthly_df = buffer_df.groupby([
+            buffer_df["Date"].dt.year,
+            buffer_df["Date"].dt.month
+        ]).last().reset_index(drop = True)
+        monthly_df = monthly_df.iloc[:, [0, 1, 5]]
+        monthly_df["Cumulative Precip (in)"] = monthly_df["Monthly Precip (in)"].cumsum()
+        print(monthly_df)
 
 
 class file_checker:
@@ -107,13 +183,22 @@ class file_checker:
 ########################################################################
 
 
-folder_path = r"I:\programming\python\riesel_file_checker\Harmel Evappan"
-f = files
-fp = f.get_files(folder_path, "32*")[0]
-fn = f.get_files(folder_path, "32*")[1]
+# folder_path = r"I:\programming\python\riesel_file_checker\Harmel Evappan"
+# f = files
+# fp = f.get_files(folder_path, "32*")[0]
+# fn = f.get_files(folder_path, "32*")[1]
 
-for path in fp:
-    # print(path)
-    r = read_excel
-    rEV = r.read_evappan(path)
-    print(rEV)
+# for path in fp:
+#     # print(path)
+#     r = read_excel
+#     rEV = r.read_evappan(path)
+#     print(rEV)
+
+root_folder = r"I:\programming\python\riesel_file_checker\Harmel\Subdaily Precip\rw9web"
+fls = files
+filepaths = fls.get_files(root_folder, "*rg*.web")[0]
+file_lst = fls.get_files(root_folder, "*rg*.web")[1]
+
+for path in filepaths:
+    read = read_excel
+    precip = read.read_precip(path)
