f82b20f8e5c65e15824fae9dd8c0cfcddff3a19b
John Sorensen <john.sorensen@usda.gov>
2026-03-30 15:37:55 -0500

    progressed in reading pdf GANS reports, will likely need to read each file individually

--- GANS.py
+++ GANS.py
@@ -4,10 +4,18 @@ import os
 import pandas as pd
 
 root = r"\\ARS-DATA\Archive\HarmelExit\riesel\GrazinglandQuality\data\GANS reports"
-graze = grazingLandQuality
+graze = grazingLandQuality.read_pdf
 
-files = glob.glob(root + "//" + "*.pdf")
-for file in files:
-    print(os.path.basename(file).split("/")[-1])
-    gan = graze.read_pdf
-    gan.read_gans(file)
+# 2012_NIRS = graze.read_NIRS(r"\\ARS-DATA\Archive\HarmelExit\riesel\GrazinglandQuality\data\GANS reports\ARSRiesel2012.pdf")
+# dec_2013_NIRS = graze.read_NIRS(r"\\ARS-DATA\Archive\HarmelExit\riesel\GrazinglandQuality\data\GANS reports\Dec2013GANS.pdf")
+
+dec2013 = graze.raw_txt(r"\\ARS-DATA\Archive\HarmelExit\riesel\GrazinglandQuality\data\GANS reports\Dec2013GANS.pdf")
+for row in dec2013:
+    print(row.split(": "))
+
+# files = glob.glob(root + "//" + "*.pdf")
+# for file in files:
+#     if os.path.basename(file).split("/")[-1] != "Riesel_SampleForm.pdf":
+#         print(os.path.basename(file).split("/")[-1])
+#         gan = graze.read_pdf
+#         gan.read_gans(file)
